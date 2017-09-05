533378b8468710f16e63cbdd23e84464d7f3eeb4
Michal Takac <michal@sld.gs>
2017-09-05 15:15:08 +0200

    initial commit and socket.io setup

---
+++ README.md
@@ -0,0 +1,16 @@
+# A-Frame Socket.io Autostreamer
+
+Trying to stream changes in scene automatically without any additional settings.
+
+### Usage
+
+    npm install
+    npm start
+
+Go to [__http://localhost:3000/__](http://localhost:3000/)
+
+Does not hot-reload. Not needed.
+
+## License
+
+This program is free software and is distributed under an [MIT License](LICENSE).
---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express')
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+server.listen(3000);
+
+app.use(express.static(__dirname + '/'));
+
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
